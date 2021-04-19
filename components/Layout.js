2ee30a3cb0e7963b7658396ab0b4f2201d8e0c47
pedrolmr <pedroluismontesinosrivera@gmail.com>
2021-04-19 00:06:59 -0400

    retrieving contentful data

--- components/Layout.js
+++ components/Layout.js
@@ -1,26 +1,26 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
 export default function Layout({ children }) {
   return (
-    <div className="layout">
+    <div className='layout'>
       <header>
-        <Link href="/">
+        <Link href='/'>
           <a>
             <h1>
-              <span>Just Add</span>
-              <span>Marmite</span>
+              <span>Recipes</span>
+              <span>App</span>
             </h1>
             <h2>Spread The Joy</h2>
           </a>
         </Link>
       </header>
 
-      <div className="page-content">
+      <div className='page-content'>
         { children }
       </div>
 
       <footer>
-        <p>Copyright 2021 Just Add Marmite :)</p>
+        <p>Copyright 2021 Recipes app</p>
       </footer>
     </div>
   )
