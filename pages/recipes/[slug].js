8f86c339c0fad1e1ac22668c4e1e90c8ed3f9697
pedrolmr <pedroluismontesinosrivera@gmail.com>
2021-04-24 20:46:14 -0400

    fallback page

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,7 @@
 import { createClient } from 'contentful';
 import Image from 'next/image';
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import Skeleton from '../../components/Skeleton';
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -19,7 +20,7 @@ export const getStaticPaths = async () => {
   })
   return {
     paths:paths,
-    fallback: false
+    fallback: true
   }
 }
 
@@ -39,6 +40,7 @@ export const getStaticProps = async ({params}) => {
 export default function RecipeDetails({recipe}) {
   const {featuredImage, title, cookingTime, ingredients, method} = recipe.fields
 
+  if(!recipe) return <Skeleton />
   // console.log(recipe)
   return (
     <div>
